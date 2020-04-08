3d6ab1bafd5cc0d03ca505afa9e132b37aa7899f
Andrew Hayworth <ahayworth@gmail.com>
2020-04-08 13:35:33 -0500

    Fix test deprecations and linting failures

--- python_awair/__init__.py
+++ python_awair/__init__.py
@@ -1,47 +1,48 @@
 """Python asyncio client for the Awair GraphQL API."""
 
-import asyncio
 import aiohttp
 import async_timeout
+
 from python_awair import const
 
-class AwairClient():
+
+class AwairClient:
     """Python asyncio client for the Awair GraphQL API."""
-    def __init__(self, access_token, session=None,
-                 timeout=aiohttp.client.DEFAULT_TIMEOUT):
-        self._headers = {"Authorization": "Bearer %s" % access_token,
-                         "Content-Type": "application/json"}
+
+    def __init__(
+        self, access_token, session=None, timeout=aiohttp.client.DEFAULT_TIMEOUT
+    ):
+        self._headers = {
+            "Authorization": "Bearer %s" % access_token,
+            "Content-Type": "application/json",
+        }
 
         self._timeout = timeout
-        if session is None:
-            self._session = aiohttp.ClientSession()
-        else:
-            self._session = session
+        self._session = session
 
-    @asyncio.coroutine
-    def user(self):
+    async def user(self):
         """Yields user data."""
-        return (yield from self._query(const.USER_QUERY))['User']
+        response = await self._query(const.USER_QUERY)
+        return response["User"]
 
-    @asyncio.coroutine
-    def devices(self):
+    async def devices(self):
         """Lists devices and locations."""
-        return (yield from self._query(const.DEVICE_QUERY))['Devices']['devices']
+        response = await self._query(const.DEVICE_QUERY)
+        return response["Devices"]["devices"]
 
-    @asyncio.coroutine
-    def air_data_latest(self, uuid, fahrenheit=False):
+    async def air_data_latest(self, uuid, fahrenheit=False):
         """Returns the latest air quality measurements."""
         variables = {
             "deviceUUID": self._quote(uuid),
             "fahrenheit": self._quote(fahrenheit),
         }
 
-        return (yield from self._query(const.LATEST_QUERY % self._query_variables(variables),
-                                       variables))['AirDataLatest']['airDataSeq']
+        response = await self._query(
+            const.LATEST_QUERY % self._query_variables(variables), variables
+        )
+        return response["AirDataLatest"]["airDataSeq"]
 
-
-    @asyncio.coroutine
-    def air_data_five_minute(self, uuid, **kwargs):
+    async def air_data_five_minute(self, uuid, **kwargs):
         """Returns the 5min summary air quality measurements."""
         # args from_date, to_date, limit, desc, fahrenheit)
         variables = {
@@ -51,12 +52,12 @@ class AwairClient():
         for key, value in kwargs.items():
             variables[key] = self._quote(value)
 
-        return (yield from self._query(const.FIVE_MIN_QUERY % self._query_variables(variables),
-                                       variables))['AirData5Minute']['airDataSeq']
+        response = await self._query(
+            const.FIVE_MIN_QUERY % self._query_variables(variables), variables
+        )
+        return response["AirData5Minute"]["airDataSeq"]
 
-
-    @asyncio.coroutine
-    def air_data_fifteen_minute(self, uuid, **kwargs):
+    async def air_data_fifteen_minute(self, uuid, **kwargs):
         """Returns the 15min summary air quality measurements."""
         # args from_date, to_date, limit, desc, fahrenheit)
         variables = {
@@ -66,12 +67,12 @@ class AwairClient():
         for key, value in kwargs.items():
             variables[key] = self._quote(value)
 
-        return (yield from self._query(const.FIFTEEN_MIN_QUERY % self._query_variables(variables),
-                                       variables))['AirData15Minute']['airDataSeq']
+        response = await self._query(
+            const.FIFTEEN_MIN_QUERY % self._query_variables(variables), variables
+        )
+        return response["AirData15Minute"]["airDataSeq"]
 
-
-    @asyncio.coroutine
-    def air_data_raw(self, uuid, **kwargs):
+    async def air_data_raw(self, uuid, **kwargs):
         """Returns raw air quality measurements."""
         # args from_date, to_date, limit, desc, fahrenheit)
         variables = {
@@ -81,48 +82,52 @@ class AwairClient():
         for key, value in kwargs.items():
             variables[key] = self._quote(value)
 
-        return (yield from self._query(const.RAW_QUERY % self._query_variables(variables),
-                                       variables))['AirDataRaw']['airDataSeq']
+        response = await self._query(
+            const.RAW_QUERY % self._query_variables(variables), variables
+        )
+        return response["AirDataRaw"]["airDataSeq"]
 
-
-    @asyncio.coroutine
-    def _query(self, query, variables=None):
+    async def _query(self, query, variables=None):
+        session = self._session or aiohttp.ClientSession()
         data = {"query": "query { %s }" % query, "variables": variables}
         with async_timeout.timeout(self._timeout.total):
-            resp = yield from self._session.post(const.AWAIR_URL, json=data, headers=self._headers)
+            resp = await session.post(
+                const.AWAIR_URL, json=data, headers=self._headers
+            )
+
             if resp.status == 400:
                 raise AwairClient.QueryError(
-                    "The query %s with variables %s is invalid." % (
-                        query, variables
-                    )
+                    "The query %s with variables %s is invalid." % (query, variables)
                 )
-            elif resp.status == 401:
+
+            if resp.status == 401:
                 raise AwairClient.AuthError(
-                    "The supplied access token is invalid or " +
-                    "does not have access to the requested data."
+                    "The supplied access token is invalid or "
+                    + "does not have access to the requested data."
                 )
-            elif resp.status == 404:
+
+            if resp.status == 404:
                 raise AwairClient.NotFoundError(
                     "The Awair API returned an unexpected HTTP 404."
                 )
 
             if resp.status == 200:
-                json = yield from resp.json()
-                data = json['data']
+                json = await resp.json()
+                data = json["data"]
 
-                if 'errors' in json:
-                    errors = json['errors']
+                if "errors" in json:
+                    errors = json["errors"]
 
                     ratelimit = False
                     for error in errors:
-                        if "Too many requests" in error['message']:
+                        if "Too many requests" in error["message"]:
                             ratelimit = True
                             break
 
                     if ratelimit:
                         raise AwairClient.RatelimitError(
-                            "The ratelimit for the Awair API has been exceeded. " +
-                            "Please try again later."
+                            "The ratelimit for the Awair API has been exceeded. "
+                            + "Please try again later."
                         )
 
                 return data
@@ -138,7 +143,7 @@ class AwairClient():
             return "false"
 
         if isinstance(arg, str):
-            return "\"%s\"" % arg
+            return '"%s"' % arg
 
         return arg
 
@@ -146,27 +151,17 @@ class AwairClient():
     def _query_variables(variables):
         return ",".join(["%s: %s" % (k, v) for (k, v) in variables.items()])
 
-
     class GenericError(Exception):
         """Generic error."""
-        pass
-
 
     class AuthError(Exception):
         """Some kind of authorization or authentication failure."""
-        pass
-
 
     class QueryError(Exception):
         """The query was somehow malformed."""
-        pass
-
 
     class NotFoundError(Exception):
         """The requested endpoint is gone."""
-        pass
-
 
     class RatelimitError(Exception):
         """The API quota was exceeded."""
-        pass
--- tests/test_python_awair.py
+++ tests/test_python_awair.py
@@ -1,148 +1,218 @@
-#pylint: skip-file
-import aiohttp
+"""Test basic python_awair functionality."""
 import asyncio
+
+import aiohttp
 import pytest
 from aioresponses import aioresponses
-from python_awair import AwairClient
-from python_awair import const
 
-loop = asyncio.get_event_loop()
+from python_awair import AwairClient, const
 
-user_response = '''{"data":{"User":{"id":"12345","email":"test@test.com","name":{"firstName":"Test","lastName":"Test"},"dob":{"year":2018,"month":11,"day":11},"sex":"MALE","tier":"Hobbyist","permissions":[{"scope":"FIFTEEN_MIN","quota":100}],"usage":[{"scope":"LATEST","counts":9}]}}}'''
-devices_response = '''{"data":{"Devices":{"devices":[{"uuid":"awair_12345","deviceType":"awair","deviceId":"12345","name":"Awair","preference":"GENERAL","macAddress":"FFFFFFFFFFFF","room":{"id":"ffffffff-ffff-ffff-ffff-ffffffffffff","name":"My Room","kind":"LIVING_ROOM","Space":{"id":"ffffffff-ffff-ffff-ffff-ffffffffffff","kind":"HOME","location":{"name":"Chicago, IL","timezone":"","lat":0.0,"lon":-0.0}}}}]}}}'''
-latest_response = '''{"data":{"AirDataLatest":{"airDataSeq":[{"timestamp":"2018-11-18T01:09:48.187Z","score":69.0,"sensors":[{"component":"TEMP","value":26.66}],"indices":[{"component":"TEMP","value":1.0}]}]}}}'''
-five_minute_response = '''{"data":{"AirData5Minute":{"airDataSeq":[{"timestamp":"2018-11-18T01:09:48.187Z","score":69.0,"sensors":[{"component":"TEMP","value":26.66}],"indices":[{"component":"TEMP","value":1.0}]}]}}}'''
-fifteen_minute_response = '''{"data":{"AirData15Minute":{"airDataSeq":[{"timestamp":"2018-11-18T01:09:48.187Z","score":69.0,"sensors":[{"component":"TEMP","value":26.66}],"indices":[{"component":"TEMP","value":1.0}]}]}}}'''
-raw_response = '''{"data":{"AirDataRaw":{"airDataSeq":[{"timestamp":"2018-11-18T01:09:48.187Z","score":69.0,"sensors":[{"component":"TEMP","value":26.66}],"indices":[{"component":"TEMP","value":1.0}]}]}}}'''
-ratelimit_response = '''{"data":null,"errors":[{"message":"Too many requests during the past 24 hours","path":["User"],"locations":[{"line":1,"column":9}]}]}'''
-
-def test_get_user():
-    awair = AwairClient('example_token')
+@pytest.fixture
+def mock_user_response():
+    """Returns a mocked user query."""
     with aioresponses() as mocked:
-        mocked.post(const.AWAIR_URL, status=200, body=user_response)
+        with open("tests/fixtures/user_response.json") as data:
+            mocked.post(const.AWAIR_URL, status=200, body=data.read())
+            yield mocked
 
-        resp = loop.run_until_complete(awair.user())
 
-        assert resp["id"] == '12345'
-        assert resp["email"] == "test@test.com"
-        assert resp["name"]["lastName"] == "Test"
-        assert resp["dob"]["day"] == 11
-        assert resp["tier"] == "Hobbyist"
-        assert resp["permissions"][0] == dict(scope="FIFTEEN_MIN", quota=100)
-        assert resp["usage"][0] == dict(scope="LATEST", counts=9)
+@pytest.fixture
+def mock_devices_response():
+    """Returns a mocked user query."""
+    with aioresponses() as mocked:
+        with open("tests/fixtures/devices_response.json") as data:
+            mocked.post(const.AWAIR_URL, status=200, body=data.read())
+            yield mocked
 
-def test_get_user_with_session():
+
+@pytest.fixture
+def mock_latest_response():
+    """Returns a mocked user query."""
+    with aioresponses() as mocked:
+        with open("tests/fixtures/latest_response.json") as data:
+            mocked.post(const.AWAIR_URL, status=200, body=data.read())
+            yield mocked
+
+
+@pytest.fixture
+def mock_five_minute_response():
+    """Returns a mocked user query."""
+    with aioresponses() as mocked:
+        with open("tests/fixtures/five_minute_response.json") as data:
+            mocked.post(const.AWAIR_URL, status=200, body=data.read())
+            yield mocked
+
+
+@pytest.fixture
+def mock_fifteen_minute_response():
+    """Returns a mocked user query."""
+    with aioresponses() as mocked:
+        with open("tests/fixtures/fifteen_minute_response.json") as data:
+            mocked.post(const.AWAIR_URL, status=200, body=data.read())
+            yield mocked
+
+
+@pytest.fixture
+def mock_raw_response():
+    """Returns a mocked user query."""
+    with aioresponses() as mocked:
+        with open("tests/fixtures/raw_response.json") as data:
+            mocked.post(const.AWAIR_URL, status=200, body=data.read())
+            yield mocked
+
+
+@pytest.fixture
+def mock_ratelimit_response():
+    """Returns a mocked user query."""
+    with aioresponses() as mocked:
+        with open("tests/fixtures/ratelimit_response.json") as data:
+            mocked.post(const.AWAIR_URL, status=200, body=data.read())
+            yield mocked
+
+
+async def test_get_user(
+    mock_user_response,
+):  # pylint: disable=unused-argument,redefined-outer-name
+    """Test that we can get a user response."""
+    awair = AwairClient("example_token")
+    resp = await awair.user()
+
+    assert resp["id"] == "12345"
+    assert resp["email"] == "test@test.com"
+    assert resp["name"]["lastName"] == "Test"
+    assert resp["dob"]["day"] == 11
+    assert resp["tier"] == "Hobbyist"
+    assert resp["permissions"][0] == dict(scope="FIFTEEN_MIN", quota=100)
+    assert resp["usage"][0] == dict(scope="LATEST", counts=9)
+
+
+async def test_get_user_with_session(
+    mock_user_response
+):  # pylint: disable=unused-argument,redefined-outer-name
+    """Test that we can get a user response with an explicit session."""
     session = aiohttp.ClientSession()
-    awair = AwairClient('example_token', session=session)
+    awair = AwairClient("example_token", session=session)
+    resp = await awair.user()
+
+    # It's a big response, just assert some things
+    assert resp["id"] == "12345"
+    assert resp["email"] == "test@test.com"
+    assert resp["name"]["lastName"] == "Test"
+    assert resp["dob"]["day"] == 11
+    assert resp["tier"] == "Hobbyist"
+    assert resp["permissions"][0] == dict(scope="FIFTEEN_MIN", quota=100)
+    assert resp["usage"][0] == dict(scope="LATEST", counts=9)
+
+
+async def test_get_devices(
+    mock_devices_response
+):  # pylint: disable=unused-argument,redefined-outer-name
+    """Test that we can get a list of devices."""
+    awair = AwairClient("example_token")
+    resp = await awair.devices()
+
+    assert resp[0]["uuid"] == "awair_12345"
+    assert resp[0]["deviceType"] == "awair"
+
+
+async def test_get_latest(
+    mock_latest_response
+):  # pylint: disable=unused-argument,redefined-outer-name
+    """Test that we can get the latest air data."""
+    awair = AwairClient("example_token")
+    resp = await awair.air_data_latest("test_device")
+    assert resp[0]["timestamp"] == "2018-11-18T01:09:48.187Z"
+    assert resp[0]["score"] == 69.0
+    assert resp[0]["sensors"][0]["component"] == "TEMP"
+    assert resp[0]["sensors"][0]["value"] == 26.66
+    assert resp[0]["indices"][0]["component"] == "TEMP"
+    assert resp[0]["indices"][0]["value"] == 1.0
+
+
+async def test_get_five_minute(
+    mock_five_minute_response
+):  # pylint: disable=unused-argument,redefined-outer-name
+    """Test that we can get the five-minute avg air data."""
+    awair = AwairClient("example_token")
+    resp = await awair.air_data_five_minute("test_device")
+    assert resp[0]["timestamp"] == "2018-11-18T01:09:48.187Z"
+    assert resp[0]["score"] == 69.0
+    assert resp[0]["sensors"][0]["component"] == "TEMP"
+    assert resp[0]["sensors"][0]["value"] == 26.66
+    assert resp[0]["indices"][0]["component"] == "TEMP"
+    assert resp[0]["indices"][0]["value"] == 1.0
+
+
+async def test_get_fifteen_minute(
+    mock_fifteen_minute_response
+):  # pylint: disable=unused-argument,redefined-outer-name
+    """Test that we can get the fifteen-minute avg air data."""
+    awair = AwairClient("example_token")
+    resp = await awair.air_data_fifteen_minute("test_device")
+    assert resp[0]["timestamp"] == "2018-11-18T01:09:48.187Z"
+    assert resp[0]["score"] == 69.0
+    assert resp[0]["sensors"][0]["component"] == "TEMP"
+    assert resp[0]["sensors"][0]["value"] == 26.66
+    assert resp[0]["indices"][0]["component"] == "TEMP"
+    assert resp[0]["indices"][0]["value"] == 1.0
+
+
+async def test_get_raw(
+    mock_raw_response
+):  # pylint: disable=unused-argument,redefined-outer-name
+    """Test that we can get the raw air data."""
+    awair = AwairClient("example_token")
+    resp = await awair.air_data_raw("test_device")
+    assert resp[0]["timestamp"] == "2018-11-18T01:09:48.187Z"
+    assert resp[0]["score"] == 69.0
+    assert resp[0]["sensors"][0]["component"] == "TEMP"
+    assert resp[0]["sensors"][0]["value"] == 26.66
+    assert resp[0]["indices"][0]["component"] == "TEMP"
+    assert resp[0]["indices"][0]["value"] == 1.0
+
+
+async def test_auth_failure():
+    """Test that we can raise on bad auth."""
+    awair = AwairClient("bad_token")
     with aioresponses() as mocked:
-        mocked.post(const.AWAIR_URL, status=200, body=user_response)
-
-        resp = loop.run_until_complete(awair.user())
-
-        # It's a big response, just assert some things
-        assert resp["id"] == '12345'
-        assert resp["email"] == "test@test.com"
-        assert resp["name"]["lastName"] == "Test"
-        assert resp["dob"]["day"] == 11
-        assert resp["tier"] == "Hobbyist"
-        assert resp["permissions"][0] == dict(scope="FIFTEEN_MIN", quota=100)
-        assert resp["usage"][0] == dict(scope="LATEST", counts=9)
-
-def test_get_devices():
-    awair = AwairClient('example_token')
-    with aioresponses() as mocked:
-        mocked.post(const.AWAIR_URL, status=200, body=devices_response)
-        resp = loop.run_until_complete(awair.devices())
-
-        assert resp[0]["uuid"] == "awair_12345"
-        assert resp[0]["deviceType"] == "awair"
-
-def test_get_latest():
-    awair = AwairClient('example_token')
-    with aioresponses() as mocked:
-        mocked.post(const.AWAIR_URL, status=200, body=latest_response)
-
-        resp = loop.run_until_complete(awair.air_data_latest("test_device"))
-        assert resp[0]['timestamp'] == '2018-11-18T01:09:48.187Z'
-        assert resp[0]['score'] == 69.0
-        assert resp[0]['sensors'][0]['component'] == 'TEMP'
-        assert resp[0]['sensors'][0]['value'] == 26.66
-        assert resp[0]['indices'][0]['component'] == 'TEMP'
-        assert resp[0]['indices'][0]['value'] == 1.0
-
-def test_get_five_minute():
-    awair = AwairClient('example_token')
-    with aioresponses() as mocked:
-        mocked.post(const.AWAIR_URL, status=200, body=five_minute_response)
-
-        resp = loop.run_until_complete(awair.air_data_five_minute("test_device"))
-        assert resp[0]['timestamp'] == '2018-11-18T01:09:48.187Z'
-        assert resp[0]['score'] == 69.0
-        assert resp[0]['sensors'][0]['component'] == 'TEMP'
-        assert resp[0]['sensors'][0]['value'] == 26.66
-        assert resp[0]['indices'][0]['component'] == 'TEMP'
-        assert resp[0]['indices'][0]['value'] == 1.0
-
-def test_get_fifteen_minute():
-    awair = AwairClient('example_token')
-    with aioresponses() as mocked:
-        mocked.post(const.AWAIR_URL, status=200, body=fifteen_minute_response)
-
-        resp = loop.run_until_complete(awair.air_data_fifteen_minute("test_device"))
-        assert resp[0]['timestamp'] == '2018-11-18T01:09:48.187Z'
-        assert resp[0]['score'] == 69.0
-        assert resp[0]['sensors'][0]['component'] == 'TEMP'
-        assert resp[0]['sensors'][0]['value'] == 26.66
-        assert resp[0]['indices'][0]['component'] == 'TEMP'
-        assert resp[0]['indices'][0]['value'] == 1.0
-
-def test_get_raw():
-    awair = AwairClient('example_token')
-    with aioresponses() as mocked:
-        mocked.post(const.AWAIR_URL, status=200, body=raw_response)
-
-        resp = loop.run_until_complete(awair.air_data_raw("test_device"))
-        assert resp[0]['timestamp'] == '2018-11-18T01:09:48.187Z'
-        assert resp[0]['score'] == 69.0
-        assert resp[0]['sensors'][0]['component'] == 'TEMP'
-        assert resp[0]['sensors'][0]['value'] == 26.66
-        assert resp[0]['indices'][0]['component'] == 'TEMP'
-        assert resp[0]['indices'][0]['value'] == 1.0
-
-def test_auth_failure():
-    awair = AwairClient('bad_token')
-    with aioresponses() as mocked:
-        mocked.post(const.AWAIR_URL, status=401, body='The supplied authentication is invalid')
+        mocked.post(
+            const.AWAIR_URL, status=401, body="The supplied authentication is invalid"
+        )
 
         with pytest.raises(AwairClient.AuthError):
-            resp = loop.run_until_complete(awair.air_data_raw("test_device"))
+            resp = await awair.air_data_raw("test_device")
             assert resp.code == 401
-            assert resp.body == 'The supplied authentication is invalid'
+            assert resp.body == "The supplied authentication is invalid"
 
-def test_bad_query():
-    awair = AwairClient('bad_token')
+
+async def test_bad_query():
+    """Test that we can raise on bad query."""
+    awair = AwairClient("bad_token")
     with aioresponses() as mocked:
         mocked.post(const.AWAIR_URL, status=400)
 
         with pytest.raises(AwairClient.QueryError):
-            resp = loop.run_until_complete(awair.air_data_raw("test_device"))
+            resp = await awair.air_data_raw("test_device")
             assert resp.code == 400
 
-def test_not_found():
-    awair = AwairClient('bad_token')
+
+async def test_not_found():
+    """Test that we can raise on 404."""
+    awair = AwairClient("bad_token")
     with aioresponses() as mocked:
         mocked.post(const.AWAIR_URL, status=404)
 
         with pytest.raises(AwairClient.NotFoundError):
-            resp = loop.run_until_complete(awair.air_data_raw("test_device"))
+            resp = await awair.air_data_raw("test_device")
             assert resp.code == 404
 
-def test_ratelimit():
-    awair = AwairClient('bad_token')
 
-    with aioresponses() as mocked:
-        mocked.post(const.AWAIR_URL, status=200, body=ratelimit_response)
+async def test_ratelimit(
+    mock_ratelimit_response
+):  # pylint: disable=unused-argument,redefined-outer-name
+    """Test that we can raise ratelimit."""
+    awair = AwairClient("bad_token")
 
-        with pytest.raises(AwairClient.RatelimitError):
-            resp = loop.run_until_complete(awair.air_data_raw("test_device"))
-            assert resp.code == 200
+    with pytest.raises(AwairClient.RatelimitError):
+        resp = await awair.air_data_raw("test_device")
+        assert resp.code == 200
